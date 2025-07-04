2ae300fe21d06ca79123fe2962c196e1071bee04
Anuradha Raj <shikhhaayadav@gmail.com>
2025-07-05 04:12:52 +0530

    sentence capitalize problem solved

--- Strings/sentenceCapitalization.js
+++ Strings/sentenceCapitalization.js
@@ -1,5 +1,8 @@
 const sentenceCapitalization = ( str ) => {
-    
+    return str.toLowerCase()
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
 }
 
-console.log(sentenceCapitalization("hello world"));
+console.log(sentenceCapitalization("hello world, whats up"));
