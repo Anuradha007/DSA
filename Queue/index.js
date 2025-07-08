0cbf3d9c83f5dbcce2b69a93c76b421f52d7cf7a
Anuradha Raj <shikhhaayadav@gmail.com>
2025-07-08 20:18:52 +0530

    Queue day-1

--- Queue/index.js
+++ Queue/index.js
@@ -1,1 +1,53 @@
- 
+ class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+ }
+
+class Queue {
+    constructor(value){
+        let newNode = new Node(value);
+        this.first = newNode;
+        this.last = newNode;
+        this.length = 1;
+    }
+
+    enqueue(value) {
+        let newNode = new Queue(value);
+
+        if(this.length === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        }
+
+        this.last.next = newNode;
+        this.last = newNode;
+        this.length++;
+        return this;
+    }
+
+    dequeue() {
+        if(this.length === 0) {
+            return undefined
+        }
+
+        let temp = this.first;
+
+        if(this.length === 1){
+            this.first = null;
+            this.last = null;
+        }
+        this.first = this.first.next;
+        temp.next  = null;
+        this.length--;
+        return temp;
+    }
+}
+
+let myQueue = new Queue(0);
+myQueue.enqueue(1)
+myQueue.enqueue(2)
+myQueue.enqueue(3)
+myQueue.dequeue();
+console.log(myQueue);
